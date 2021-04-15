b1fb8ccbdfcb020a53beccdb0f34930bf77b1b93
Rokibul Hossan <rokibulhossan32@gmail.com>
2021-04-16 00:17:14 +0600

    front-end complete

--- src/Widgets/Widgets.js
+++ src/Widgets/Widgets.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import "./Widgets.css";
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed
+} from "react-twitter-embed";
+import SearchIcon from '@material-ui/icons/Search';
 
 const Widgets = () => {
     return (
         <div className="widgets">
-            <h2>Widgets</h2>
+            <div className="widgets_input">
+                <SearchIcon className="widgets_searchIcon" />
+                <input placeholder=" Search Twitter" type="text" />
+            </div>
+            <div className="widgets_widgetContainer">
+                <h2> What's happening </h2>
+            <TwitterTweetEmbed tweetId={'1382403878704926724'}/>
+            <TwitterTimelineEmbed 
+            sourceType="profile"
+            screenName="rockzz"
+            option={{height:400}}/>
+             <TwitterShareButton
+    url={'https://www.facebook.com/rock.rokib'}
+    options={{ text: "#reactjs is awesome", via: "rock.rokib" }} /*ekhane amr witter er HossanRokibul bosate hobe tahole amar sob post show korbe*/
+
+  />
+            </div>
         </div>
     );
 };
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC-Z4r3aILdPc2SKPXcFPlIHOIQ4EYVoEE",
+    authDomain: "twitter-clone-6cf80.firebaseapp.com",
+    projectId: "twitter-clone-6cf80",
+    storageBucket: "twitter-clone-6cf80.appspot.com",
+    messagingSenderId: "853460574622",
+    appId: "1:853460574622:web:d366b1967d4fbe6263368f",
+    measurementId: "G-LXCWQCD2CC"
+  };
